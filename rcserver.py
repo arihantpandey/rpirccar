71ee6b5983edd42e995e3f5adddf14769d8f937e
Arihant Pandey <arihantpandey2002@gmail.com>
2022-08-16 23:01:15 -0700

    add motor forward/backward buttons

--- rcserver.py
+++ rcserver.py
@@ -5,43 +5,46 @@ from flask import Flask, render_template, Response
 import datetime
 import time
 
-dataPin=[i for i in range(2,28)]
-for dp in dataPin: GPIO.setup(dp,GPIO.IN)
+
+
 
 # signal pin setup
 GPIO.setmode(GPIO.BCM) # Broadcom SOC channel designation
+
+dataPin=[i for i in range(2,28)]
+for dp in dataPin: GPIO.setup(dp,GPIO.IN)
 
 LED0 = 10
 LED1 = 9
 LED2 = 25
 
 # Motor Drive Interface Definition 
-ENA = 13    #L298 enable A
-ENB = 20    #//L298 enable B
-IN1 = 19    #Motor Interface 1
-IN2 = 16    #Motor Interface 2
-IN3 = 21    #Motor Interface 3
-IN4 = 26    #Motor Interface 4
+ENA = 13	#L298 enable A
+ENB = 20	#//L298 enable B
+IN1 = 19	#Motor Interface 1
+IN2 = 16	#Motor Interface 2
+IN3 = 21	#Motor Interface 3
+IN4 = 26	#Motor Interface 4
 
 #Ultrasonic Interface Definition
-ECHO = 4    #Ultrasonic receiver pin  
+ECHO = 4	#Ultrasonic receiver pin  
 TRIG = 17   #Ultrasonic transmitter pin'
 
 #Infrared sensor interface definition
-IR_R = 18    #Infrared patrol line on the right side of the car
-IR_L = 27    #Infrared patrol line on the left side of the car
-IR_M = 22    #Infrared obstacle avoidance in the middle of the car
-IRF_R = 23    #The car follows the right infrared
-IRF_L = 24    #The car follows the left infrared
+IR_R = 18	#Infrared patrol line on the right side of the car
+IR_L = 27	#Infrared patrol line on the left side of the car
+IR_M = 22	#Infrared obstacle avoidance in the middle of the car
+IRF_R = 23	#The car follows the right infrared
+IRF_L = 24	#The car follows the left infrared
 global Cruising_Flag
-Cruising_Flag = 0    #//当前循环模式
+Cruising_Flag = 0	#//当前循环模式
 global Pre_Cruising_Flag
-Pre_Cruising_Flag = 0     #//预循环模式
+Pre_Cruising_Flag = 0 	#//预循环模式
 
 global RevStatus
 RevStatus = 0
 global TurnAngle
-TurnAngle=0
+TurnAngle=0;
 global Golength
 Golength=0
 buffer = ['00','00','00','00','00','00']
@@ -94,15 +97,69 @@ GPIO.setup(IRF_L,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(TRIG,GPIO.OUT,initial=GPIO.LOW) #Ultrasonic module transmitter pin setting trig
 GPIO.setup(ECHO,GPIO.IN,pull_up_down=GPIO.PUD_UP) #Ultrasonic module receiver pin setting echo
 
+####CAR FUNCTIONS####
 
-def    Open_Light():
-    GPIO.output(LED0,False)
-    time.sleep(1)
-    print("light on")
+def	Open_Light():
+	GPIO.output(LED0,False)
+	time.sleep(1)
+	print("light on")
 
-def    Close_Light():
-    GPIO.output(LED0,True)
-    time.sleep(1)
+def	Close_Light():
+	GPIO.output(LED0,True)
+	time.sleep(1)
+    
+def Motor_Forward():
+	print('motor forward')
+	GPIO.output(ENA,True)
+	GPIO.output(ENB,True)
+	GPIO.output(IN1,True)
+	GPIO.output(IN2,False)
+	GPIO.output(IN3,True)
+	GPIO.output(IN4,False)
+	GPIO.output(LED1,False)
+	GPIO.output(LED2,False)
+	
+def Motor_Backward():
+	print('motor_backward')
+	GPIO.output(ENA,True)
+	GPIO.output(ENB,True)
+	GPIO.output(IN1,False)
+	GPIO.output(IN2,True)
+	GPIO.output(IN3,False)
+	GPIO.output(IN4,True)
+	GPIO.output(LED1,True)
+	GPIO.output(LED2,False)
+	
+def Motor_TurnLeft():
+	print('motor_turnleft')
+	GPIO.output(ENA,True)
+	GPIO.output(ENB,True)
+	GPIO.output(IN1,True)
+	GPIO.output(IN2,False)
+	GPIO.output(IN3,False)
+	GPIO.output(IN4,True)
+	GPIO.output(LED1,False)
+	GPIO.output(LED2,True) 
+def Motor_TurnRight():
+	print('motor_turnright')
+	GPIO.output(ENA,True)
+	GPIO.output(ENB,True)
+	GPIO.output(IN1,False)
+	GPIO.output(IN2,True)
+	GPIO.output(IN3,True)
+	GPIO.output(IN4,False)
+	GPIO.output(LED1,False)
+	GPIO.output(LED2,True) 
+def Motor_Stop():
+	print('motor_stop')
+	GPIO.output(ENA,False)
+	GPIO.output(ENB,False)
+	GPIO.output(IN1,False)
+	GPIO.output(IN2,False)
+	GPIO.output(IN3,False)
+	GPIO.output(IN4,False)
+	GPIO.output(LED1,True)
+	GPIO.output(LED2,True)
 
 """
 
@@ -118,8 +175,8 @@ templateData={
     'data':data,
 }
 """
-dataPin=[i for i in range(2,28)]
 
+#update data from GPIO input
 def getData():
     data=[]
     for i,dp in enumerate(dataPin): 
@@ -136,7 +193,7 @@ def index():
     timeString=now.strftime("%Y-%m-%d %H:%M")
     data=getData()
     templateData={
-        'title':'Raspberry Pi 3B+ Web Controller',
+        'title':'Raspberry Pi 3B+ RC Car Web Controller',
         'time':timeString,
         'data':data,
     }
@@ -150,6 +207,14 @@ def handleRequest(actionid):
         Open_Light()
     elif(actionid == "dig2off"):
         Close_Light()
+    if(actionid == "dig3on"):
+        Motor_Forward()
+    elif(actionid == "dig3off"):
+        Motor_Stop()
+    if(actionid == "dig4on"):
+        Motor_Backward()
+    elif(actionid == "dig4off"):
+        Motor_Stop()
 
     return "OK 200"   
                               
